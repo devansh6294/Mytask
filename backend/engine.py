c8f716944df1b0f441d460f22fbdec304c99fd0b
devansh6294 <gevanshdupta1234@gmail.com>
2026-07-14 21:52:19 +0530

    Update engine.py

--- backend/engine.py
+++ backend/engine.py
@@ -1,7 +1,8 @@
 from sqlalchemy import create_engine 
 from sqlalchemy.orm import sessionmaker
+import os
 
-url="postgresql://postgres:Devansh%406294@localhost:5432/final_taskmanager"
+url = os.environ.get("DATABASE_URL", "postgresql://postgres:Devansh%406294@localhost:5432/final_taskmanager")
 engine=create_engine(url,echo=True)
 session=sessionmaker(bind=engine)
 
